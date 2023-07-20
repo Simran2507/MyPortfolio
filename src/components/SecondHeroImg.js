a6985cbf8c0874b9d51192f590a7365ab977174a
Simran2507 <simranjaiswal2530@gmail.com>
2023-07-20 13:03:18 +0530

    Converting class based component to functional based component

--- src/components/SecondHeroImg.js
+++ src/components/SecondHeroImg.js
@@ -1,18 +1,16 @@
 import './SecondHeroImg.css'
 import React from 'react'
-import {Component} from "react";
 
-class SecondHeroImg  extends Component{
-    render(){
-        return (
-            <div className="heroimgTwo">
-               <div className="heading">
-                    <h1>{this.props.heading}</h1>
-                    <p>{this.props.text}</p>
-                </div>
-            </div>
-        )
-    }
+const SecondHeroImg = ({heading, text}) => {
+  return (
+    <div className="heroimgTwo">
+        <div className="heading">
+            <h1>{heading}</h1>
+            <p>{text}</p>
+        </div>
+    </div>
+    
+  )
 }
 
 export default SecondHeroImg
